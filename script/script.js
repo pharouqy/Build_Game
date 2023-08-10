862d3420d921dbddd4e9344c712cd3dd6c1d69e4
pharouqy <pharouqy@gmail.com>
2023-08-10 14:28:00 +0100

    JavaScript logic done !!!

---
+++ script/script.js
@@ -0,0 +1,38 @@
+const player1 = document.getElementById("player1");
+const result = document.getElementById("result");
+const player2 = document.getElementById("player2");
+
+handlePlay();
+
+function randomHands() {
+  const hands = ["✊", "✌️", "🤚"];
+  const randomIndex1 = Math.floor(Math.random() * 3);
+  const randomIndex2 = Math.floor(Math.random() * 3);
+  player1.textContent = hands[randomIndex1];
+  player2.textContent = hands[randomIndex2];
+}
+
+function play() {
+  if (player1.textContent === player2.textContent) {
+    return (result.textContent = "It's a Tie!");
+  } else {
+    if (
+      (player1.textContent === "✊" && player2.textContent === "✌️") ||
+      (player1.textContent === "✌️" && player2.textContent === "🤚") ||
+      (player1.textContent === "🤚" && player2.textContent === "✊")
+    ) {
+      return (result.textContent = "Player 1 wins !!!");
+    } else {
+      return (result.textContent = "Player 2 wins !!!");
+    }
+  }
+}
+
+function handlePlay() {
+  const btn = document.getElementById("btn");
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    randomHands();
+    play();
+  });
+}
